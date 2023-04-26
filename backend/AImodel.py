30deaad9efbfdf7a03ad9b01543e2d7319752718
NotsoJharedtroll23 <abrahambourne@hotmail.com>
2023-04-25 21:52:12 -0700

    REDACT: nuevo archivo para el modelo personalizado para el proyecto

---
+++ backend/AImodel.py
@@ -0,0 +1,23 @@
+''''
+            LEER CADA UNO PARA ENTRENAR MODELO HIPOTETICO
+* https://huggingface.co/docs/transformers/main_classes/pipelines
+* https://towardsdatascience.com/teach-an-ai-model-to-write-like-shakespeare-for-free-a9e6a307139
+* https://neptune.ai/blog/how-to-use-google-colab-for-deep-learning-complete-tutorial
+* https://huggingface.co/blog/sentiment-analysis-python
+* https://towardsdatascience.com/scraping-reddit-data-1c0af3040768
+* https://www.geeksforgeeks.org/scraping-reddit-using-python/
+* https://huggingface.co/docs/transformers/generation_strategies
+* https://huggingface.co/blog/sentiment-analysis-python
+* https://huggingface.co/docs/transformers/training
+* https://huggingface.co/google/flan-t5-base
+* https://wandb.ai/authors/Kaggle-NLP/reports/Kaggle-s-NLP-Text-Classification--VmlldzoxOTcwNTc
+* https://jmlr.org/papers/volume21/20-074/20-074.pdf Exploring the Limits of Transfer Learning with a Unified Text-to-Text Transformer
+* https://arxiv.org/pdf/2210.11416.pdf Scaling Instruction-Finetuned Langueage Models
+'''
+
+# TODO redactar codigo para utilizar en Google Colab para entrenar el modelo
+'''
+La idea principal es detectar si un texto es ofensivo y 
+    de ser verdadero, reescribir el texto para que deje de serlo;
+    De lo contrario el texto se mantiene igual para no hacer el proceso de okis...
+'''
